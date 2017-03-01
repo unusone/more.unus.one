c420b1e6efa6036d8d5eaafddcf570b9d76fe2b7
unus.one <info@unus.one>
2017-03-01 22:52:47 +0100

    Add uo-json

---
+++ uo-json/blog_post.js
@@ -0,0 +1,8 @@
+{
+  "post" : [
+    { "file" : "post1.html", "link" : "post1.html" },
+    { "file" : "post2.html", "link" : "post2.html" },
+    { "file" : "post3.html", "link" : "post3.html" },
+    { "file" : "post4.html", "link" : "post4.html" }
+   ]
+}
---
+++ uo-json/menu_item.js
@@ -0,0 +1,9 @@
+{
+  "menu" : [
+    { "title" : "Home", "link" : "#" },
+    { "title" : "About", "link" : "#About" },
+    { "title" : "News", "link" : "#News" },
+    { "title" : "Blog", "link" : "#Blog" },
+    { "title" : "Link", "link" : "#Link" }
+   ]
+}
